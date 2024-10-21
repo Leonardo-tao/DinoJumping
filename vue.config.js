7d0d128759262df730450e23b476eeaa638dcfaa
Tao811 <13045197623@163.com>
2024-10-21 19:14:13 +0800

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/DinoJumping/'
+    : '/',
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      // 修改网站标题
+      args[0].title = '恐龙游戏 - 在线玩Dino'
+      return args
+    })
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+      })
+      return definitions
+    })
+  }
 })
